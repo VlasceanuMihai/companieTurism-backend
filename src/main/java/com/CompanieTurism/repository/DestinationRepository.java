ddad3d3567e6d2d835b6d369f2df3bb1cf4ecace
Vlasceanu Mihai <you@example.com>
2021-05-26 19:05:28 +0300

    Create endpoint to get the destinations and the hotels - refactor

--- src/main/java/com/CompanieTurism/repository/DestinationRepository.java
+++ src/main/java/com/CompanieTurism/repository/DestinationRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DestinatieRepository extends JpaRepository<Destination, Integer> {
+public interface DestinationRepository extends JpaRepository<Destination, Integer> {
 }
